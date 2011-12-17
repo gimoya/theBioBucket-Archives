74f3db26114bc8b57bead7c942f175005da27040
gimoya <kay.cichini@gmail.com>
2011-12-17 22:48:39 +0100

    Update R/IPtoXY.R

--- R/IPtoXY.R
+++ R/IPtoXY.R
@@ -8,9 +8,9 @@ IPtoXY <- function(x) {
    api_return <- readLines(URL_IP, warn = F)
    str_elements <- gsub("[^[:alnum:].]", "",
                         strsplit(api_return, "\"")[[1]])
-   lon <- str_elements[grep("longitude", str_elements) + 1]
-   lat <- str_elements[grep("latitude", str_elements) + 1]
-   return(paste(lon, lat, sep = ";"))
+   return(paste(str_elements[grep("longitude", str_elements)+1],
+                str_elements[grep("latitude", str_elements)+1],
+                sep = ";"))
 }
 
 # Example:
