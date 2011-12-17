4cb88191f1dcaf52649639088c5f899340565fec
gimoya <kay.cichini@gmail.com>
2011-12-17 21:38:55 +0100

    Update R/IPtoXY.R

--- R/IPtoXY.R
+++ R/IPtoXY.R
@@ -8,8 +8,8 @@ IPtoXY <- function(x) {
    api_return <- readLines(URL_IP, warn = F)
    str_elements <- gsub("[^[:alnum:].]", "",
                         strsplit(api_return, "\"")[[1]])
-   lon <- str_elements[grep("longitude", str_elements)+1]
-   lat <- str_elements[grep("latitude", str_elements)+1]
+   lon <- str_elements[grep("longitude", str_elements) + 1]
+   lat <- str_elements[grep("latitude", str_elements) + 1]
    return(paste(lon, lat, sep = ";"))
 }
 
