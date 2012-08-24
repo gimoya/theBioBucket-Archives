df20df975418b3988a4a13cbd203ee25f353efc9
gimoya <kay.cichini@gmail.com>
2012-08-24 23:03:31 +0200

    some more edits..

--- R/Functions/GScholarScraper_3.1.R
+++ R/Functions/GScholarScraper_3.1.R
@@ -14,12 +14,18 @@
 # Logical, should a table be writen to user default directory?
 # if TRUE a CSV-file with hyperlinks to the publications will be created.
 #
-# Caveat: if a submitted search string gives more than 1000 hits there seem
-# to be some problems (I guess I'm being stopped by Google for roboting the site..)
-#
 # Difference to version 3:
 # added "since" argument - define year since when publications should be returned.. 
 # added field "YEAR" to output
+#
+# Caveat: if a submitted search string gives more than 1000 hits there seem
+# to be some problems (I guess I'm being stopped by Google for roboting the site..)
+#
+# And, there is an issue with this error message:
+# > Error in htmlParse(URL): 
+# > error in creating parser for http://scholar.google.com/scholar?q
+# I haven't figured out his one yet..
+
 
 GScholar_Scraper <- function(input, since = 1990, write = F) {
 
@@ -37,6 +43,7 @@ GScholar_Scraper <- function(input, since = 1990, write = F) {
     h1 <- xpathSApply(doc, "//div[@id='gs_ab_md']", xmlValue)
     h2 <- strsplit(h1, " ")[[1]][2] 
     num <- as.integer(sub("[[:punct:]]", "", h2))
+    cat("Number of hits: ", num, "\n----\n", "If this number is far from the returned results\nsomething might have gone wrong..\n\n...", sep = "")
     
     # If there are no results, stop and throw an error message:
     if (num == 0 | is.na(num)) {
@@ -70,7 +77,7 @@ GScholar_Scraper <- function(input, since = 1990, write = F) {
         # ..to be extended for individual needs
         
         dat <- data.frame(TITLES = tit, PUBLICATION = pub, 
-                          YEAR = gsub("\\s", "", str_extract(pub, "\\s\\d{4}\\s")),
+                          YEAR = as.integer(gsub(".*\\s(\\d{4})\\s.*", "\\1", pub)),
                           LINKS = lin)
         return(dat)
     }
@@ -87,10 +94,22 @@ GScholar_Scraper <- function(input, since = 1990, write = F) {
 }
 
 # EXAMPLES:
+input <- "intitle:metapopulation"
+df <- GScholar_Scraper(input, since = 1980)
+nrow(df)
+hist(df$YEAR)
+
 input <- "allintitle:live on mars"
 GScholar_Scraper(input, since = 2006)
 
 input <- "allintitle:ziggy stardust"
 GScholar_Scraper(input, write = T)
 
-# ERROR with message: input <- "allintitle:crazyshit"; GScholar_Scraper(input)
+# ERROR with message:
+input <- "allintitle:crazyshit"
+GScholar_Scraper(input)
+
+# CAVEAT, Google blocks automated requests at about the 1000th hit:
+input <- "metapopulation"
+df <- GScholar_Scraper(input, since = 1980)
+nrow(df)
